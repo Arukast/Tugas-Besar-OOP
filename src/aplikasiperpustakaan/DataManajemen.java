5c3164ec07152499854867cf3892de81394e5aea
Arukast <altatubagus@gmail.com>
2024-12-04 16:30:31 +0000

    adding all class from diagram class

---
+++ src/aplikasiperpustakaan/DataManajemen.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasiperpustakaan;
+
+/**
+ *
+ * @author Tubagus Alta
+ */
+abstract class DataManajemen {
+    public abstract void inputData();
+    public abstract void hapusData();
+    public abstract void updateData();
+}
