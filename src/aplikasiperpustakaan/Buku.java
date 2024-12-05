cabc28f0fb52e8be8a93abef3a794bad4f192f8d
Arukast <altatubagus@gmail.com>
2024-12-05 08:20:09 +0000

    adding progres and image of class diagram

--- src/aplikasiperpustakaan/Buku.java
+++ src/aplikasiperpustakaan/Buku.java
@@ -8,6 +8,41 @@ package aplikasiperpustakaan;
  *
  * @author Tubagus Alta
  */
-public class Buku {
+public class Buku extends DataManajemen {
+    private String idBuku;
+    private String judulBuku;
+    private String penulis;
+    private String genre;
+    private String bahasa;
+    private int jumlahBuku;
     
+    public Buku(String idBuku, String judulBuku, String penulis, String genre, String bahasa, int jumlahBuku){
+        this.idBuku = idBuku;
+        this.judulBuku = judulBuku;
+        this.penulis = penulis;
+        this.genre = genre;
+        this.bahasa = bahasa;
+        this.jumlahBuku = jumlahBuku;
+    }
+
+    @Override
+    public void inputData() {
+        
+    }
+
+    @Override
+    public void hapusData() {
+        
+    }
+
+    @Override
+    public void updateData() {
+        
+    }
+    
+    
+    
+            
+            
+          
 }
